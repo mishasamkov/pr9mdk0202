f229c873497ccbb6b240e0b770d380a90ad055e5
student-a502 <student-a502@STUD-RM15-A502>
2025-12-15 16:15:01 +0500

    Созданный классы для приложения. samkov

---
+++ Classes/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerTelegramBot_Classes
+{
+    public class Users
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string NotificationTime { get; set; }
+        public string Login { get; set; }
+    }
+}
